cc5f0feaac89833769a707070414b0a8b0587fb2
jclagoria <lagoria79@gmail.com>
2020-06-09 21:17:55 -0300

    dto's project

---
+++ back-end/reddit-app/src/main/java/ar/com/reddit/dto/LogoutRequest.java
@@ -0,0 +1,17 @@
+package ar.com.reddit.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.validation.constraints.NotBlank;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class LogoutRequest {
+
+    @NotBlank
+    private String refreshToken;
+
+}
