26ec1c8d73b7e97cd7c1faf144cf7fdae86dd7fb
jclagoria <lagoria79@gmail.com>
2020-06-15 00:05:37 -0300

    first commit angular-reddit

---
+++ front-end/reddit-clone/src/app/auth/auth.guard.ts
@@ -0,0 +1,3 @@
+import {Injectable} from '@angular/core';
+import {CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree, Router} from '@angular/router';
+import {Observable} from 'rxjs';
